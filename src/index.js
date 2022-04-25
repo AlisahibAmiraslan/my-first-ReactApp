b17aa5bf27d4bd2ffb085c9d9fa11408bfd04206
Alisahib Amiraslan <=>
2022-04-26 02:00:22 +0300

    uptade codes

--- src/index.js
+++ src/index.js
@@ -1,17 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import SingleComment from "./SingleComment";
+import profile1 from "./image/img1.jpg";
+import profile2 from "./image/img2.jpeg";
+import profile3 from "./image/img3.jpg";
+import UserCard  from "./UserCard";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+const App = () =>{
+  return(
+    <div className="ui comments">
+      <UserCard>
+     <SingleComment name="John" date="Today at 5:00PM" text="You are incredible" picture={profile1}/>
+     </UserCard>
+     <UserCard><SingleComment name="Ali" date="Today at 6:00PM" text="Great Effort Friends" picture={profile2}/></UserCard>
+     
+     <UserCard><SingleComment name="Werner" date="Today at 7:00PM" text="Hello that is amaizing" picture={profile3}/></UserCard>
+    </div>
+  )
+}
+
+
+ReactDOM.render(
+  <App/>,
+  document.getElementById("root")
+)
